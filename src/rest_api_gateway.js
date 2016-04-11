746cccade6ff176370e7b64d7040fabb71ac8259
Axel Hodler <derxor@gmail.com>
2016-04-11 20:33:24 +0200

    no need for this method anymore

--- src/rest_api_gateway.js
+++ src/rest_api_gateway.js
@@ -1,9 +1,6 @@
 var ledgerGateway = require('./ledger_gateway');
 
 module.exports = {
-  fetchOverview: function(req, res) {
-    res.send('overview');
-  },
   getAll: function(req, res) {
     res.send(ledgerGateway.allBalances());
   }
