5d24ea89c061aa42eee7450b82195ccea805d6f1
Axel Hodler <derxor@gmail.com>
2016-04-11 20:13:57 +0200

    test get all ledger entries api method

--- src/rest_api_gateway.js
+++ src/rest_api_gateway.js
@@ -1,5 +1,10 @@
+var ledgerGateway = require('./ledger_gateway');
+
 module.exports = {
   fetchOverview: function(req, res) {
     res.send('overview');
+  },
+  getAll: function(req, res) {
+    res.send(ledgerGateway.allBalances());
   }
 };
--- src/rest_api_gateway_test.js
+++ src/rest_api_gateway_test.js
@@ -1,14 +1,21 @@
-var api = require('./rest_api_gateway');
 var td = require('testdouble');
+var ledgerGatewayTd = td.replace('./ledger_gateway');
+var api = require('./rest_api_gateway');
+var LedgerEntry = require('./model/ledger_entry');
 
 describe('rest api gateway', function() {
-  it('can provide an overview of tokens for each', function() {
+  it('gets all ledgerentries', function() {
     function Response(){};
     Response.prototype.send = function(){};
     var responseSpy = td.object(Response);
+    var allLedgerEntriesStub = [
+      new LedgerEntry('accountId', 22),
+      new LedgerEntry('accountId2', 23)
+    ];
+    td.when(ledgerGatewayTd.allBalances()).thenReturn(allLedgerEntriesStub);
 
-    var overview = api.fetchOverview({}, responseSpy);
+    var overview = api.getAll({}, responseSpy);
 
-    td.verify(responseSpy.send('overview'));
+    td.verify(responseSpy.send(allLedgerEntriesStub));
   });
 });
