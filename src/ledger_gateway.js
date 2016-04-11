dec41c342878b1f22f9d37c08089770b00944f7c
Axel Hodler <derxor@gmail.com>
2016-04-11 19:47:52 +0200

    reduce duplication

--- src/ledger_gateway.js
+++ src/ledger_gateway.js
@@ -2,13 +2,16 @@ var LedgerEntry = require('./model/ledger_entry');
 var ethereumGateway = require('./boundaries/ethereum_gateway');
 var accountGateway = require('./boundaries/account_gateway');
 
+var buildLedgerEntry = function(accountId) {
+  return new LedgerEntry(accountId, ethereumGateway.balanceOf(accountId));
+};
 module.exports = {
   balanceOf: function(accountId) {
-    return new LedgerEntry(accountId, ethereumGateway.balanceOf(accountId));
+    return buildLedgerEntry(accountId);
   },
   allBalances: function() {
     return accountGateway.fetchAll().map(function(accountId) {
-      return new LedgerEntry(accountId, ethereumGateway.balanceOf(accountId));
+      return buildLedgerEntry(accountId);
     });
   }
 };
