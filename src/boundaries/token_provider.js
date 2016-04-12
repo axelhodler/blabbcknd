1f08cbb81912ee94fd21118b465efb37880527ab
Axel Hodler <derxor@gmail.com>
2016-04-12 20:14:45 +0200

    explain api usage

--- src/boundaries/token_provider.js
+++ src/boundaries/token_provider.js
@@ -4,6 +4,7 @@ module.exports = {
   sign: function(payload, secret) {
     return jwt.sign(payload, secret);
   },
+  // e.g. use .verifiedContent().username to get the username in the payload
   verifiedContent: function(token, secret) {
     return jwt.verify(token, secret);
   }
