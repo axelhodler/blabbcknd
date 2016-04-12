3a08d2c82abcb8724451395beca0fef9bbb96f4e
Axel Hodler <derxor@gmail.com>
2016-04-12 20:36:36 +0200

    no need to pass the secret from outside

--- src/boundaries/token_provider.js
+++ src/boundaries/token_provider.js
@@ -1,11 +1,13 @@
 var jwt = require('jsonwebtoken');
 
+var secret = 'secret';
+
 module.exports = {
-  sign: function(payload, secret) {
+  sign: function(payload) {
     return jwt.sign(payload, secret);
   },
   // e.g. use .verifiedContent().username to get the username in the payload
-  verifiedContent: function(token, secret) {
+  verifiedContent: function(token) {
     return jwt.verify(token, secret);
   }
 };
