3f3a4eeeb9d72b13f72181a6bd78a5c321bc60ec
Axel Hodler <derxor@gmail.com>
2016-04-12 22:28:23 +0200

    account structure has changed

--- src/boundaries/account_gateway.js
+++ src/boundaries/account_gateway.js
@@ -1,9 +1,9 @@
 var Account = require('../model/account');
 
 var accounts = [
-  new Account('mail1@test.com', 'pw1', '0xf19f24526c0f804b9ddb7d9d80521f9e7e638819'),
-  new Account('mail2@test.com', 'pw2', '0x7e3098562ba4e34a904c5b412197763d418966bb'),
-  new Account('mail3@test.com', 'pw3', '0x957fcd52e975bde95b61e5f6057eab44d916a211')
+  new Account(1, 'mail1@test.com', 'pw1', '0xf19f24526c0f804b9ddb7d9d80521f9e7e638819'),
+  new Account(2, 'mail2@test.com', 'pw2', '0x7e3098562ba4e34a904c5b412197763d418966bb'),
+  new Account(3, 'mail3@test.com', 'pw3', '0x957fcd52e975bde95b61e5f6057eab44d916a211')
 ];
 
 module.exports = {
