265b023754731bd0d9bc45966787a5541b3f97e1
Axel Hodler <derxor@gmail.com>
2016-04-12 23:24:46 +0200

    improve test structure

--- src/rest_api_gateway_test.js
+++ src/rest_api_gateway_test.js
@@ -12,33 +12,38 @@ describe('rest api gateway', function() {
     responseSpy = td.object(Response);
   });
 
-  it('gets all ledgerentries', function() {
-    var allLedgerEntriesStub = [
-      new LedgerEntry('ethereumAddress', 22),
-      new LedgerEntry('accountId2', 23)
-    ];
-    td.when(ledgerGatewayTd.allBalances()).thenReturn(allLedgerEntriesStub);
+  describe('ledger', function() {
+    it('gets all ledgerentries', function() {
+      var allLedgerEntriesStub = [
+        new LedgerEntry('ethereumAddress', 22),
+        new LedgerEntry('accountId2', 23)
+      ];
+      td.when(ledgerGatewayTd.allBalances()).thenReturn(allLedgerEntriesStub);
 
-    var overview = api.getAll({}, responseSpy);
+      var overview = api.getAll({}, responseSpy);
 
-    td.verify(responseSpy.send(allLedgerEntriesStub));
+      td.verify(responseSpy.send(allLedgerEntriesStub));
+    });
+
+    it('gets balance for accountid', function() {
+      var requestStub = { params : { id:1 }};
+      td.when(ledgerGatewayTd.balanceOf(1)).thenReturn(100);
+
+      api.getBalanceFor(requestStub, responseSpy);
+
+      td.verify(responseSpy.send(100));
+    });
   });
 
-  it('gets balance for accountid', function() {
-    var requestStub = { params : { id:1 }};
-    td.when(ledgerGatewayTd.balanceOf(1)).thenReturn(100);
+  describe('auth', function() {
+    it('can provide tokens to users', function() {
+      var requestStub = { body: { email: 'foo@bar.io', password: 'pw'}};
+      td.when(authUser.login(requestStub.body.email, requestStub.body.password)).thenReturn('validToken');
 
-    api.getBalanceFor(requestStub, responseSpy);
+      api.login(requestStub, responseSpy);
 
-    td.verify(responseSpy.send(100));
+      td.verify(responseSpy.send('validToken'));
+    });
   });
 
-  it('can provide tokens to users', function() {
-    var requestStub = { body: { email: 'foo@bar.io', password: 'pw'}};
-    td.when(authUser.login(requestStub.body.email, requestStub.body.password)).thenReturn('validToken');
-
-    api.login(requestStub, responseSpy);
-
-    td.verify(responseSpy.send('validToken'));
-  });
 });
