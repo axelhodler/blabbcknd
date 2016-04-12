fe552d5359b5169dfa6e5723e689641de9b2b13d
Axel Hodler <derxor@gmail.com>
2016-04-12 21:29:01 +0200

    rm string duplication

--- src/auth_user_test.js
+++ src/auth_user_test.js
@@ -5,30 +5,33 @@ var Account = require('./model/account');
 
 describe('auth user', function() {
   it('returns token if credentials are valid', function() {
-    td.when(tokenProvider.sign('email@host.io')).thenReturn('validToken');
-    var accountStub = new Account(1, 'email@host.io', 'password');
-    td.when(accountGateway.fetchAccountByEmail('email@host.io')).thenReturn(accountStub);
+    var EMAIL = 'email@host.io';
+    td.when(tokenProvider.sign(EMAIL)).thenReturn('validToken');
+    var accountStub = new Account(1, EMAIL, 'password');
+    td.when(accountGateway.fetchAccountByEmail(EMAIL)).thenReturn(accountStub);
 
-    var token = authUser.login('email@host.io', 'password');
+    var token = authUser.login(EMAIL, 'password');
 
     expect(token).to.equal('validToken');
   });
 
   it('returns token if credentials are valid - triangulation', function() {
-    td.when(tokenProvider.sign('foo@bar.io')).thenReturn('aValidToken');
-    var accountStub = new Account(1, 'foo@bar.io', 'pw');
-    td.when(accountGateway.fetchAccountByEmail('foo@bar.io')).thenReturn(accountStub);
+    var EMAIL = 'foo@bar.io';
+    td.when(tokenProvider.sign(EMAIL)).thenReturn('aValidToken');
+    var accountStub = new Account(1, EMAIL, 'pw');
+    td.when(accountGateway.fetchAccountByEmail(EMAIL)).thenReturn(accountStub);
 
-    var token = authUser.login('foo@bar.io', 'pw');
+    var token = authUser.login(EMAIL, 'pw');
 
     expect(token).to.equal('aValidToken');
   });
 
   it('does not return a token for invalid credentials', function() {
-    var accountStub = new Account(1, 'email@host.io', 'correctPassword');
-    td.when(accountGateway.fetchAccountByEmail('email@host.io')).thenReturn(accountStub);
+    var EMAIL = 'email@host.io';
+    var accountStub = new Account(1, EMAIL, 'correctPassword');
+    td.when(accountGateway.fetchAccountByEmail(EMAIL)).thenReturn(accountStub);
 
-    var token = authUser.login('email@host.io', 'incorrectPassword');
+    var token = authUser.login(EMAIL, 'incorrectPassword');
 
     expect(token).to.equal(undefined);
     td.verify(tokenProvider.sign('payload'), {times: 0});
