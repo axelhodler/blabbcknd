47a3465ca85e9500baffb77fe03b7fec8f27a083
Axel Hodler <derxor@gmail.com>
2016-04-16 21:08:26 +0200

    can move tokens via rest api

--- src/rest_api_write_to_ledger.js
+++ src/rest_api_write_to_ledger.js
@@ -1,6 +1,8 @@
 var Request = require('./boundaries/wrappers/request');
 var Response = require('./boundaries/wrappers/response');
 var authUser = require('./actions/auth_user');
+var writeToLedger = require('./actions/write_to_ledger');
+var accountGateway = require('./boundaries/account_gateway');
 
 var checkTokenValidity = function(req) {
   return authUser.isTokenValid(req.authorizationHeader());
@@ -10,10 +12,20 @@ var unauthorizedResponse = function(res) {
   res.sendUnauthorized();
 };
 
+var moveTokens = function(req, res) {
+  writeToLedger.moveTokens(
+    accountGateway.fetchAccountByEmail(
+      authUser.mailInDecoded(
+        req.authorizationHeader()))
+      .getEtherAddress(),
+    req.body().to,
+    req.body().amount);
+  return res;
+};
 module.exports = {
   moveTokens: function(request, response) {
     checkTokenValidity(request)
-      ? response.sendOk()
+      ? moveTokens(request, response).sendOk()
       : unauthorizedResponse(response);
   }
 };
--- src/rest_api_write_to_ledger_test.js
+++ src/rest_api_write_to_ledger_test.js
@@ -1,7 +1,10 @@
 var authUser = td.replace('./actions/auth_user');
+var accountGateway = td.replace('./boundaries/account_gateway');
+var writeToLedger = td.replace('./actions/write_to_ledger');
 var api = require('./rest_api_write_to_ledger');
 var Request = require('./boundaries/wrappers/request');
 var Response = require('./boundaries/wrappers/response');
+var Account = require('./model/account');
 
 describe('write to ledger', function() {
   var requestStub,
@@ -19,8 +22,20 @@ describe('write to ledger', function() {
   });
 
   it('moves tokens if authorized', function() {
-    api.moveTokens(stubValidToken(requestStub, authUser), responseSpy);
+    td.when(authUser.mailInDecoded('validToken')).thenReturn('mail@mail.com');
+    var stubbedAccount = new Account(1, 'mail@mail.com', 'irrelevant', 'fromEtherAddr');
+    td.when(accountGateway.fetchAccountByEmail('mail@mail.com')).thenReturn(stubbedAccount);
+    var stubbedRequest = stubValidToken(requestStub, authUser);
+    td.when(stubbedRequest.body()).thenReturn(
+      {
+        'to':'toEtherAddress',
+        'amount':100
+      }
+    );
 
+    api.moveTokens(stubbedRequest, responseSpy);
+
+    td.verify(writeToLedger.moveTokens('fromEtherAddr', 'toEtherAddress', 100));
     td.verify(responseSpy.sendOk());
   });
 });
