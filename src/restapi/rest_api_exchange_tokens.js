670d60c50e04434774a0705b174b2acc2bb5965f
Axel Hodler <derxor@gmail.com>
2016-05-04 16:29:41 +0200

    always return a reponse

--- src/restapi/rest_api_exchange_tokens.js
+++ src/restapi/rest_api_exchange_tokens.js
@@ -3,5 +3,7 @@ var writeToLedger = require('./../actions/write_to_ledger');
 module.exports = {
   toEuro: function(request, response) {
     writeToLedger.destroyTokens(request.body().recipient, request.body().amount);
+
+    return response;
   }
 };
