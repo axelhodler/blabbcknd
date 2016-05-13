624b500f68bfd35120baa4efdd34f7482a48a03d
Axel Hodler <derxor@gmail.com>
2016-05-13 23:07:18 +0200

    rm unused method

--- src/testhelpers/helpers.js
+++ src/testhelpers/helpers.js
@@ -5,12 +5,6 @@ afterEach(function() {
   td.reset();
 });
 
-global.stubInvalidToken = function(requestStub, authUser) {
-  td.when(authUser.isTokenValid('invalidToken')).thenReturn(false);
-  td.when(requestStub.authorizationHeader()).thenReturn('invalidToken');
-  return requestStub;
-};
-
 global.stubValidToken = function(requestStub, authUser) {
   td.when(authUser.isTokenValid('validToken')).thenReturn(true);
   td.when(requestStub.authorizationHeader()).thenReturn('validToken');
