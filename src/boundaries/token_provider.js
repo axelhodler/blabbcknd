a1209d023529376f5b6c117f05444fd125b4d7bb
Axel Hodler <derxor@gmail.com>
2016-05-14 12:51:07 +0200

    stub tokensecret

--- src/boundaries/token_provider.js
+++ src/boundaries/token_provider.js
@@ -1,17 +1,16 @@
 var jwt = require('jsonwebtoken');
-
-var secret = 'secret';
+var tokenSecret = require('./tokensecret');
 
 module.exports = {
   sign: function(payload) {
-    return jwt.sign(payload, secret);
+    return jwt.sign(payload, tokenSecret.get());
   },
   // e.g. use .verifiedContent().username to get the username in the payload
   // will throw if token invalid
   verifiedContent: function(token) {
-    return jwt.verify(token, secret);
+    return jwt.verify(token, tokenSecret.get());
   },
   getSecret: function() {
-    return secret;
+    return tokenSecret.get();
   }
 };
