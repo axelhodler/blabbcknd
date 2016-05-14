807b23f2ba8270fd07ef2c87b29452d0564a20a2
Axel Hodler <derxor@gmail.com>
2016-05-14 12:45:41 +0200

    cover tokenprovider with tests

--- src/boundaries/token_provider_test.js
+++ src/boundaries/token_provider_test.js
@@ -5,4 +5,15 @@ describe('TokenProvider', function() {
     expect(tokenProvider.sign('payload'))
       .to.equal('eyJhbGciOiJIUzI1NiJ9.cGF5bG9hZA.4GMt2k_zZryxhKgC8_HvdSZtYxyEyDa0AFIL-n60a8M');
   });
+
+  it('verifies tokens', function () {
+    var payload = 'somePayload';
+    var token = tokenProvider.sign(payload);
+
+    expect(tokenProvider.verifiedContent(token)).to.equal(payload);
+  });
+
+  it('provides the secret', function() {
+    expect(tokenProvider.getSecret()).to.equal('secret');
+  });
 });
