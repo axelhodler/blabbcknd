c5bcb2e4b0a9b3e207ceced667b019d0102b0efe
Axel Hodler <derxor@gmail.com>
2016-05-15 12:08:11 +0200

    remove duplication

--- src/restapi/request_router.js
+++ src/restapi/request_router.js
@@ -1,25 +1,33 @@
-var Request = require('./../boundaries/delivery/request_wrapper');
-var Response = require('./../boundaries/delivery/response_wrapper');
+var RequestWrapper = require('./../boundaries/delivery/request_wrapper');
+var ResponseWrapper = require('./../boundaries/delivery/response_wrapper');
 
 var restApiGateway = require('./rest_api_gateway');
 var readLedger = require('./rest_api_read_ledger');
 var writeToLedger = require('./rest_api_write_to_ledger');
 var exchangeTokens = require('./rest_api_exchange_tokens');
 
+var wrapRequest = function(request) {
+  return new RequestWrapper(request);
+};
+
+var wrapResponse = function(response) {
+  return new ResponseWrapper(response);
+};
+
 module.exports = {
   toEuro: function(req, res) {
-    exchangeTokens.toEuro(new Request(req), new Response(res));
+    exchangeTokens.toEuro(wrapRequest(req), wrapResponse(res));
   },
   getAll: function(req, res) {
-    readLedger.getAll(new Request(req), new Response(res));
+    readLedger.getAll(wrapRequest(req), wrapResponse(res));
   },
   getBalanceFor: function(req, res) {
-    readLedger.getBalanceFor(new Request(req), new Response(res));
+    readLedger.getBalanceFor(wrapRequest(req), wrapResponse(res));
   },
   login: function(req, res) {
-    restApiGateway.login(new Request(req), new Response(res))
+    restApiGateway.login(wrapRequest(req), wrapResponse(res));
   },
   moveTokens: function(req, res) {
-    writeToLedger.moveTokens(new Request(req), new Response(res));
+    writeToLedger.moveTokens(wrapRequest(req), wrapResponse(res));
   }
 };
