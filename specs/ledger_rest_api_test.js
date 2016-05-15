0c6df1df9b33f0ffa8fa923bce4714526175f0a0
Axel Hodler <derxor@gmail.com>
2016-05-15 12:58:49 +0200

    extend specs

--- specs/ledger_rest_api_test.js
+++ specs/ledger_rest_api_test.js
@@ -2,6 +2,8 @@ var request = require('supertest');
 var server = require('../src/boundaries/delivery/ledger_rest_api').start();
 
 describe('Rest API', function () {
+  var token;
+
   it('receives token with valid credentials', function (done) {
     request(server)
       .post('/auth')
@@ -13,7 +15,25 @@ describe('Rest API', function () {
       )
       .expect(200)
       .end(function(error, response) {
-        expect(response.text).to.contain('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9');
+        token = response.text;
+        expect(token).to.contain('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9');
+        done();
+      });
+  });
+
+  var accountId;
+
+  it('can read all ledger entries', function(done) {
+    this.timeout(5000);
+    request(server)
+      .get('/ledgers')
+      .set('Authorization', token)
+      .expect(200)
+      .end(function(error, response) {
+        accountId = response.body[0];
+        expect(response.body.length).to.equal(10);
+        console.log(accountId);
+        setTimeout(done, 5000);
         done();
       });
   });
