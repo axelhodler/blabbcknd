8aabb0837a202feecdf85a3857820216c44f04af
Axel Hodler <derxor@gmail.com>
2016-05-22 21:25:02 +0200

    improve stepdefinition location

--- features/step_definitions/add_member_to_project.js
+++ features/step_definitions/add_member_to_project.js
@@ -7,15 +7,6 @@ module.exports = function() {
     project.addMember(member);
   };
 
-  this.Given(/^a project owned by "([^"]*)" with initial tokens of (\d+)$/, function (owner, initialTokens) {
-    this.project = new Project(owner, +initialTokens);
-  });
-
-  this.Given(/^a project owner "([^"]*)"$/, function (owner_address) {
-    projectOwner = owner_address;
-    this.project = new Project(owner_address, 0);
-  });
-
   this.Given(/^a project member "([^"]*)"$/, function (member) {
     addMemberToProject(member, this.project);
   });
@@ -34,9 +25,5 @@ module.exports = function() {
 
   this.Then(/^member "([^"]*)" has (\d+) tokens$/, function (projectMember, expectedAmount) {
     expect(this.project.tokenAmountFor(projectMember)).to.equal(+expectedAmount);
-  });
-
-  this.Then(/^the project he participates in has (\d+) tokens$/, function (availableTokens) {
-    expect(this.project.availableTokens()).to.equal(+availableTokens);
   });
 };
--- features/step_definitions/creating_projects.js
+++ features/step_definitions/creating_projects.js
@@ -16,11 +16,24 @@ module.exports = function() {
     initialTokens = toInt(tokens);
   });
 
+  this.Given(/^a project owned by "([^"]*)" with initial tokens of (\d+)$/, function (owner, initialTokens) {
+    this.project = new Project(owner, +initialTokens);
+  });
+
+  this.Given(/^a project owner "([^"]*)"$/, function (owner_address) {
+    projectOwner = owner_address;
+    this.project = new Project(owner_address, 0);
+  });
+
   this.When(/^blocklab creates a project$/, function () {
     this.project = new Project(owner, initialTokens);
   });
 
   this.Then(/^the project\-owner holds (\d+) tokens$/, function (expectedTokenAmount) {
     expect(this.project.availableTokens()).to.equal(toInt(expectedTokenAmount));
+  });
+
+  this.Then(/^the project he participates in has (\d+) tokens$/, function (availableTokens) {
+    expect(this.project.availableTokens()).to.equal(+availableTokens);
   });
 };
